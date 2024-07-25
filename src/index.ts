ea1cda6e88ad3e0b1fc96c1c60099843bcd46e89
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-25 16:18:36 +0700

    chore: Add TypeScript configuration, build scripts, and update npm dependency to latest stable version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,79 @@
-export const myName: string = 'Hamzah';
+// * Annotation in depth
+// export const myName: string = 'Hamzah';
 
-console.log(myName);
+// console.log(myName);
+
+// function stringMessage(message: string): string {
+//   return message;
+// }
+// const message = stringMessage('Hallo TypeScript');
+// console.log(message);
+
+// * Any Type
+// let color: any = 'crimson';
+// color = 20;
+// color = true;
+// color.toUpperCase();
+// console.log(color);
+
+// * Function Parameter Annotation
+// Regular Func
+// function addOne(num: number) {
+//   return num + 1;
+// }
+
+// const newNumber = addOne(4);
+// console.log(newNumber);
+
+// arrow function
+// const double = (x: number, y: number) => x * y;
+// const res = double(2, 10);
+// console.log(res);
+
+// * Default params value
+// function greet(person: string = 'Anonymous') {
+//   return `hello ${person}`;
+// }
+// const resGreet = greet();
+// console.log(resGreet);
+
+// * return annotations (Regular)
+// regular function
+// function double1(x: number): number {
+//   return x * x;
+// }
+// const res3 = double1(3);
+// console.log(res3);
+// arrow function
+// const double2 = (x: number): number => x * x;
+// const res = double2(2);
+// console.log(res);
+
+/* 
+Void in TypeScript is a type that represent the absence of any value.
+it ids often used as a return type for function that do not return a value
+*/
+
+// Void Function
+// function printMessage(message: string): void {
+//   console.log('This is my message:', message);
+// }
+// printMessage('Hallo semuanya');
+
+/* 
+Never, The never keyword is used to indicate that a function will not return anything, or that a variable can never a have a value. the never type is useful for indicating that certain code paths should never be reached, or that certain values are impossible. it can help catch errors at compile-time instead of runtime.
+
+Never keyword often used for:
+1. a function that always throws an error
+2. a function that has an infinite loop
+3. a variable that can never have a value
+*/
+
+// example never keyword
+// function throwError(msg: string): never {
+//   throw new Error(msg);
+// }
+
+/* 
+Array types, array are a type of object that can store multiple values of the same data type. arrays in typescript are typed, which means you can specify the type of values that an array can hold
+*/
